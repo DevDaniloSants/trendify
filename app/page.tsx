399ec0e3cbec0ed6d1998582b955dcc0d44df0fe
Danilo Santos <dev.danilosantos@gmail.com>
2025-03-09 16:39:17 -0300

    chore: add Husky, lint-staged, commit-msg, Prettier, and Prettier plugin for Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <button className="bg-red-400 p-4">teste</button>;
+    return (
+        <button className="rounded-2xl bg-red-400 p-4 font-sans text-2xl font-black text-white">
+            teste
+        </button>
+    );
 };
 
 export default Home;
