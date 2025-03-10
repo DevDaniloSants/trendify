262fc44a0c833e0652b6cf51f69ec18cda9ffa1c
Danilo Santos <dev.danilosantos@gmail.com>
2025-03-10 20:57:52 -0300

    refactor: improve Container component by adding className support

--- app/_components/container.tsx
+++ app/_components/container.tsx
@@ -1,9 +1,14 @@
 interface ContainerProps {
     children: React.ReactNode;
+    className?: string;
 }
 
-const Container = ({ children }: ContainerProps) => {
-    return <div className="w-full space-y-6 xl:w-[1200px]">{children}</div>;
+const Container = ({ children, className }: ContainerProps) => {
+    return (
+        <div className={`w-full space-y-6 xl:w-[1200px] ${className}`}>
+            {children}
+        </div>
+    );
 };
 
 export default Container;
