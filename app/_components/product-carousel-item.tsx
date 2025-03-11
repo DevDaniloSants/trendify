1843dd98eafb3ca9c3681e5ab6ecd5719cab35b9
Danilo Santos <dev.danilosantos@gmail.com>
2025-03-11 04:13:49 -0300

    style: adjust ProductCarouselItem layout for better responsiveness

--- app/_components/product-carousel-item.tsx
+++ app/_components/product-carousel-item.tsx
@@ -9,7 +9,7 @@ const ProductCarouselItem = ({ product }: ProductCarouselItemProps) => {
     return (
         <CarouselItem
             key={product.id}
-            className="basis-1/1 md:basis-1/3 xl:basis-1/4"
+            className="basis-1/2 md:basis-1/3 lg:basis-1/4"
         >
             <ProductItem {...product} />
         </CarouselItem>
