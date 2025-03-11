4554b7034a843b8c67c1af220e8203138cc5695b
Danilo Santos <dev.danilosantos@gmail.com>
2025-03-10 23:36:20 -0300

    feat: add sectionHeader component

---
+++ app/_components/section-header.tsx
@@ -0,0 +1,47 @@
+import { cn } from '../_lib/utils';
+
+export const SectionHeader = ({
+    children,
+    className,
+}: {
+    children: React.ReactNode;
+    className?: string;
+}) => {
+    return <div className={cn('space-y-2', className)}>{children}</div>;
+};
+
+export const SectionTitle = ({
+    children,
+    className,
+}: {
+    children: React.ReactNode;
+    className?: string;
+}) => {
+    return (
+        <div className="flex items-center gap-4">
+            <span className="bg-destructive h-10 w-5 rounded-sm" />
+            <h2 className={cn('text-destructive font-bold', className)}>
+                {children}
+            </h2>
+        </div>
+    );
+};
+
+export const SectionDescription = ({
+    children,
+    className,
+}: {
+    children: React.ReactNode;
+    className?: string;
+}) => {
+    return (
+        <p
+            className={cn(
+                'text-primary text-xl sm:text-2xl md:text-3xl lg:text-4xl',
+                className
+            )}
+        >
+            {children}
+        </p>
+    );
+};
