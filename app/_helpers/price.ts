e769b830b36c2b6c1193c3e060a578543b7863fe
Danilo Santos <dev.danilosantos@gmail.com>
2025-03-10 23:21:10 -0300

    feat: add formatCurrency and price helpers

---
+++ app/_helpers/price.ts
@@ -0,0 +1,30 @@
+import { Product } from '../_data-access/interfaces/product';
+import { formatCurrency } from './formatCurrency';
+
+export const applyDiscountToProduct = (
+    product: Product
+): { discountedPrice: string; discountPercentage: number } => {
+    const discountPercentage = Math.floor(Math.random() * 50);
+
+    if (discountPercentage === 0) {
+        return {
+            discountedPrice: formatCurrency(product.price),
+            discountPercentage: 0,
+        };
+    }
+
+    const discount = product.price * (discountPercentage / 100);
+
+    const newPrice = product.price - discount;
+
+    const discountedPrice = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+    }).format(newPrice);
+
+    return {
+        discountedPrice,
+        discountPercentage,
+    };
+};
