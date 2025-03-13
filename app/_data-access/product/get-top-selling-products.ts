e435fb039fc3d47ed63f08bd0dc10e56669ef87c
Danilo Santos <dev.danilosantos@gmail.com>
2025-03-13 01:13:40 -0300

    feat: add filterValidImages to getTopSellingProducts

--- app/_data-access/product/get-top-selling-products.ts
+++ app/_data-access/product/get-top-selling-products.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { filterValidImages } from '@/app/_helpers/is-valid-image-url';
 import { formatCurrency } from '../../_helpers/formatCurrency';
 
 import { GetProducDTO, Product } from '../interfaces/product';
@@ -7,19 +8,25 @@ import { GetProducDTO, Product } from '../interfaces/product';
 export const getTopSellingProducts = async (): Promise<GetProducDTO[]> => {
     const data = await fetch(
         process.env.API_URL +
-            '/products/?price_min=50&price_max=1000&offset=10&limit=10'
+            '/products/?price_min=20&price_max=100000000&offset=10&limit=100'
     ).then((res) => res.json());
 
     if (!data) {
         throw new Error('Failed to fetch data');
     }
 
-    const products = data.map((product: Product): GetProducDTO => {
-        return {
-            ...product,
-            price: formatCurrency(product.price),
-        };
-    });
+    const filteredProducts = filterValidImages(data);
+
+    console.log(filteredProducts);
+
+    const products = filteredProducts
+        .map((product: Product): GetProducDTO => {
+            return {
+                ...product,
+                price: formatCurrency(product.price),
+            };
+        })
+        .slice(0, 10);
 
     return products;
 };
